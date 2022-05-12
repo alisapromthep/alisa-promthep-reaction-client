296792f009311e6a1a711718a35ae024c377866b
Alisa Promthep <alisa.promthep@gmail.com>
2022-05-11 22:39:24 -0400

    project folders set-up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
+import LoginPage from './pages/LoginPage/LoginPage';
+import ProfilePage from './pages/ProfilePage/ProfilePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={HomePage}/>
+          <Route path="/login" component={LoginPage}/>
+          <Route path="/:userId" component={ProfilePage}/> 
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
---
+++ src/pages/ProfilePage/ProfilePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProfilePage(props) {
+    return (
+        <div>
+            This is profile page
+        </div>
+    );
+}
+
+export default ProfilePage;
