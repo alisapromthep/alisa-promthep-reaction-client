cbac03fcbf235523020b429853d615b5c9177d05
Alisa Promthep <alisa.promthep@gmail.com>
2022-05-14 12:50:52 -0400

    calandar setup

--- src/components/CalendarComponent/CalendarComponent.js
+++ src/components/CalendarComponent/CalendarComponent.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './CalendarComponent.scss';
-import 'react-calendar/dist/Calendar.css';
+// import DatePicker from 'react-datepicker';
+// import 'react-datepicker/dist/react-datepicker.css';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 function CalendarComponent(props) {
     return (
         <div>
             <Calendar />
+
+            
         </div>
     );
 }
